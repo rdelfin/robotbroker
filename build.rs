2fb9612a05adc97615432c79a3f55b56d9ab61e0
Ricardo Delfin <me@rdelfin.com>
2021-01-24 17:49:56 +0000

    Added capnp output as modules

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     ::capnpc::CompilerCommand::new()
+        .src_prefix("capnp")
         .file("capnp/core.capnp")
         .file("capnp/node.capnp")
         .run()?;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 pub mod broker_internal;
 mod node;
 
+pub mod core_capnp {
+    include!(concat!(env!("OUT_DIR"), "/capnp/core_capnp.rs"));
+}
+pub mod node_capnp {
+    include!(concat!(env!("OUT_DIR"), "/capnp/node_capnp.rs"));
+}
+
 pub use self::node::{start, ProgramNode};
