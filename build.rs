91552bd0eba5f0d8b4993bcb59c5e2de6562da6d
Ricardo Delfin <me@rdelfin.com>
2021-01-24 23:19:07 +0000

    Removed all capnp code

--- build.rs
+++ build.rs
@@ -1,9 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ::capnpc::CompilerCommand::new()
-        .src_prefix("capnp")
-        .file("capnp/core.capnp")
-        .file("capnp/node.capnp")
-        .run()?;
     tonic_build::compile_protos("proto/broker.proto")?;
     tonic_build::compile_protos("proto/node.proto")?;
     Ok(())
--- capnp/node.capnp
+++
@@ -1,5 +0,0 @@
-@0xc7706b82c4699733;
-
-interface Node {
-    heartbeat @0 () -> (ok :Bool);
-}
